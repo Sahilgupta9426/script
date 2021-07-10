411efd30fa9a90e9caeb9814b42b9b19061b6214
sahil <sahil.sg39@gmail.com>
2021-07-10 18:19:51 +0530

    new fil3

--- mod1.js
+++ mod1.js
@@ -6,6 +6,44 @@ a = document.all;
 // Array.from(a).forEach(function(element){
 //     console.log(element);
 // })
-a = document.links[0];
+a = document.links.length;
+
+
+// let b=document.links;
 // use document.images and document.scripts and print the list of images and scripts on an html page
-console.log(a);
+// console.log(c);
+
+
+
+// let str="sahil.com";
+// for(i=0;i<a;i++){
+//     let str2=document.links[i];
+//     let str3=str2.toString();
+    
+
+//     if(str==str3){
+//         console,log(str)
+    
+// }
+// else{
+//     console.log("not found")
+// }
+// }
+
+
+
+
+
+
+
+
+// Challenge Accepted
+let site = document.links;
+
+Array.from(site).forEach(function(word){
+
+    if(word.href.includes("sahil")){
+        console.log(word.href);
+    }
+
+}); 
